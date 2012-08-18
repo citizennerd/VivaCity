0221961465b400907d4d0d09878bf30c7cfacedf
Marco Montanari <sirmmo@gmail.com>
2012-08-18 10:58:20 +0000

    extras

--- ui/views.py
+++ ui/views.py
@@ -1,4 +1,24 @@
 # Create your views here.
 from django.shortcuts import render_to_response
+
 def index(request):
     return render_to_response('index.html')
+
+def proxy(request, path):
+    import httplib2
+    conn = httplib2.Http()
+    url = path
+    if request.method == 'GET':
+        url_ending = '%s?%s' % (url, request.GET.urlencode())
+        url = "http://" + url_ending
+        response, content = conn.request(url, request.method)
+    elif request.method == 'POST':
+        url = "http://" + url
+        data = request.POST.urlencode()
+        response, content = conn.request(url, request.method, data)
+    return HttpResponse(content, status = int(response['status']), mimetype = response['content-type'])
+    
+def get_google_places(request, BB, types):
+    return None
+
+
