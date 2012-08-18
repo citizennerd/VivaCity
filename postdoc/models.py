06d50e515fd1f0064f2b5dc92f32bcc0cbdd83d0
Marco Montanari <sirmmo@gmail.com>
2012-08-18 00:55:10 +0000

    postdoc init

--- postdoc/models.py
+++ postdoc/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.gis.db import models as gmodels
 
-# Create your models here.
+class DataModel(models.Model):
+    name = models.CharField(max_length=255)
+    super = models.ForeignKey('DataModel', blank=True, null=True)
+    is_base = models.BooleanField(default=False)
+    geo_representation = models.CharField(max_length=100, blank=True, null=True)
+    abstract = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.name
+    
+class DataModelAttribute(models.Model):
+    model = models.ForeignKey(DataModel, related_name="attributes")
+    name = models.CharField(max_length=255)
+    data_type = models.ForeignKey(DataModel)
+    
+    def __str__(self):
+        return self.name
+    
+class DataInstance(models.Model):
+    data_type = models.ForeignKey(DataModel, related_name = "instances")
+    geometry = gmodels.GeometryCollectionField(null=True, blank=True)
+    
+    def __str__(self):
+        return "%s:%s" % (self.data_type, self.id, )
+    
+class DataInstanceAttribute(models.Model):
+    instance = models.ForeignKey(DataInstance, related_name="attributes")
+    attribute = models.ForeignKey(DataModelAttribute)
+    value = models.TextField()
+    
+    
--- postdoc/views.py
+++ postdoc/views.py
@@ -1,1 +1,52 @@
-# Create your views here.
+from django.http import HttpResponse
+
+from postdoc.models import *
+
+import json
+
+def get_instance_json(instance_id):
+    di = DataInstance.objects.get(id=instance_id)
+    jdi = {}
+    jdi['id'] = di.id
+    jdi['data_type'] = di.data_type.name
+    jdi['data_type_id'] = di.data_type.id
+    if di.geometry is not None and di.geometry != "":
+        jdi['geometry'] = di.geometry.geojson(); 
+    jdi['attributes'] = []
+    for attribute in di.attributes:
+        jdia = {}
+        jdia['name'] = attribute.attribute.name
+        jdia['data_type'] = attribute.attribute.data_type.name
+        jdia['data_type_id'] = attribute.attribute.data_type.id
+        jdia['value'] = attribute.attribute.value
+        jdi['attributes'].append(jdia)
+    return jdi
+    
+def get_data_json(datamodel_id):
+    dm = DataModel.objects.get(id=datamodel_id)
+    jdm = {}
+    
+    jdm['name'] = dm.name
+    jdm['id'] = dm.id
+    if dm.super is not None:
+        jdm['super'] = dm.super.name
+        jdm['super_id'] = dm.super.id
+    jdm['is_base'] = dm.is_base
+    jdm['geo_representation'] = dm.geo_representation
+    jdm['abstract'] = dm.abstract
+    jdm['attributes'] = []
+    for attribute in dm.attributes:
+        jdma = {}
+        jdma['name'] = attribute.name
+        jdma['data_type'] = attribute.data_type.name
+        jdma['data_type_id'] = attribute.data_type.id
+        jdm['attributes'].append(jdma)
+    
+    return jdm
+
+
+def http_get_data_json(request, id):
+    return HttpResponse(json.dumps(get_data_json(id)), content_type="text/json")
+
+def http_get_instance_json(request, id):
+    return HttpResponse(json.dumps(get_instance_json(id)), content_type="text/json")
